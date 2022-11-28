72653d0add71d49a1618fed5f7d00351ba231164
OMAR <omar.ahmed.khalaf2006@gmail.com>
2022-11-29 01:15:49 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { TestimonialsComponent } from './main/testimonials/testimonials.component';
+import { TeamComponent } from './main/team/team.component';
+import { RecentPostsComponent } from './main/recent-posts/recent-posts.component';
+import { PricingComponent } from './main/pricing/pricing.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { ServicesComponent } from './main/services/services.component';
+import { AboutComponent } from './main/about/about.component';
+import { HeroComponent } from './hero/hero.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : "home" , component : HeroComponent},
+{path : "" , component : HeroComponent},
+{path : "about" , component : AboutComponent},
+{path : "services" , component : ServicesComponent},
+{path : "contact" , component : ContactComponent},
+{path : "protfolio" , component : PortfolioComponent},
+{path : "pricing" , component : PricingComponent},
+{path : "recent-posts" , component : RecentPostsComponent},
+{path : "team" , component : TeamComponent},
+{path : "testimonials" , component : TestimonialsComponent},
+{path : "404" , component : NotFoundComponent},
+{path : "**" , component : NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutComponent } from './main/about/about.component';
+import { ServicesComponent } from './main/services/services.component';
+import { TestimonialsComponent } from './main/testimonials/testimonials.component';
+import { PortfolioComponent } from './main/portfolio/portfolio.component';
+import { TeamComponent } from './main/team/team.component';
+import { PricingComponent } from './main/pricing/pricing.component';
+import { RecentPostsComponent } from './main/recent-posts/recent-posts.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    PortfolioComponent,
+    TeamComponent,
+    PricingComponent,
+    RecentPostsComponent,
+    ContactComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
